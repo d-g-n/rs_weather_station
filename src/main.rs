4896bfeaa4c147c5c936a6699da2773b70a1c323
Declan Neilson <declan.g.neilson@gmail.com>
2021-03-19 17:10:42 +0000

    Added initial pass of code from rppal demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::{Gpio, Trigger, Level};
+use rppal::system::DeviceInfo;
+
+// Gpio uses BCM pin numbering.
+const GPIO_RADIO: u8 = 17;
+
+fn handle(level: Level) -> Result<(), Box<dyn Error>> {
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(GPIO_RADIO)?.into_input()
+        .set_async_interrupt(Trigger::Both, handle);
+
+    // Blink the LED by setting the pin's logic level high for 500 ms.
+    pin.set_high();
+    thread::sleep(Duration::from_millis(500));
+    pin.set_low();
+
+    Ok(())
 }
