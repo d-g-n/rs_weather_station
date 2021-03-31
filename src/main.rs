1e2823285d3c356aa22c530dda77eea2b858ff6f
Declan Neilson <declan.g.neilson@gmail.com>
2021-04-01 00:22:19 +0100

    Add writing of stats via the api and arbitrary stat writing via json

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,110 @@ use weather::IngestionState;
 use std::error::Error;
 
 use chrono::prelude::*;
-use influxdb::Client;
+use influxdb::InfluxDbWriteable;
+use influxdb::{Client, Timestamp, WriteQuery};
 use log::{error, info};
+use rocket::http::Status;
+use rocket::{Data, State};
+use rocket_contrib::json::Json;
 use rppal::gpio::{Gpio, Trigger};
 use rppal::system::DeviceInfo;
+use serde::Deserialize;
+use serde_json::Value;
 use std::collections::HashMap;
+use std::io::Read;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[derive(Debug, PartialEq, Deserialize)]
+struct EspStatusRequest {
+    battery_voltage: f32,
+    seesaw_capacitive: f32,
+    seesaw_temperature: f32,
+    bme_temp: f32,
+    bme_pressure: f32,
+    bme_altitude: f32,
+}
+
+#[derive(InfluxDbWriteable, Copy, Clone)]
+struct EspStatus {
+    time: DateTime<Utc>,
+    battery_voltage: f32,
+    seesaw_capacitive: f32,
+    seesaw_temperature: f32,
+    bme_temp: f32,
+    bme_pressure: f32,
+    bme_altitude: f32,
+}
+
+#[post("/esp", format = "json", data = "<esp_status_request>")]
+fn esp_post(influx_client: State<Client>, esp_status_request: Json<EspStatusRequest>) -> Status {
+    info!("print test {:?}", esp_status_request);
+
+    let esp_status = EspStatus {
+        time: Utc::now(),
+        battery_voltage: esp_status_request.battery_voltage,
+        seesaw_capacitive: esp_status_request.seesaw_capacitive,
+        seesaw_temperature: esp_status_request.seesaw_temperature,
+        bme_temp: esp_status_request.bme_temp,
+        bme_pressure: esp_status_request.bme_pressure,
+        bme_altitude: esp_status_request.bme_altitude,
+    };
+
+    async_std::task::block_on(async {
+        let _write_result = influx_client.query(&esp_status.into_query("esp")).await;
+    });
+
+    Status::Ok
+}
+
+#[post("/espnew", format = "json", data = "<data>")]
+fn esp_post_new(influx_client: State<Client>, data: Data) -> Status {
+    let mut buffer: String = String::new();
+
+    data.open().read_to_string(&mut buffer).unwrap();
+
+    let v: Value = serde_json::from_str(&*buffer).unwrap();
+
+    let rt: WriteQuery =
+        Timestamp::Milliseconds(Utc::now().timestamp_millis() as u128).into_query("esp");
+
+    let query_to_write: WriteQuery = v.as_object().unwrap().iter().fold(rt, |wq, (key, value)| {
+        if value.is_f64() {
+            wq.add_field(key, value.as_f64().unwrap())
+        } else if value.is_i64() {
+            wq.add_field(key, value.as_i64().unwrap())
+        } else if value.is_u64() {
+            wq.add_field(key, value.as_u64().unwrap())
+        } else {
+            wq
+        }
+    });
+
+    async_std::task::block_on(async {
+        let _write_result = influx_client.query(&query_to_write).await;
+    });
+
+    info!("print NEW TEST {:?}", v);
+
+    Status::Ok
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
     let client = Client::new(
+        format!(
+            "http://{}:{}",
+            APP_CONFIG.influx_host, APP_CONFIG.influx_port
+        ),
+        APP_CONFIG.influx_database.clone(),
+    );
+
+    let client_other = Client::new(
         format!(
             "http://{}:{}",
             APP_CONFIG.influx_host, APP_CONFIG.influx_port
@@ -63,7 +152,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .manage(client_other)
+        .mount("/api", routes![index, esp_post, esp_post_new])
+        .launch();
 
     info!(
         "Started rs_weather_station on {}.",
